3989ed42f9968778b01cf42538e73fe35f4086ad
Alex Bloom <lehanbloom@gmail.com>
2020-05-17 13:55:06 +0300

    Day 1. Ex. 1. Completed MS 5, which is to add a route for getting a specific post by id.

--- Day 1/Exercise 1/api.py
+++ Day 1/Exercise 1/api.py
@@ -8,9 +8,17 @@ app = Flask(__name__)
 @app.route("/posts")
 def get_posts():
     posts = requests.get("https://jsonplaceholder.typicode.com/posts")
-    posts_to_json = posts.json()
-    response = app.response_class(response=json.dumps(posts_to_json), status=200, mimetype='application/json')
+    posts_in_json = posts.json()
+    response = app.response_class(response=json.dumps(posts_in_json), status=200, mimetype='application/json')
     return response
+
+@app.route("/posts/<post_id>")
+def get_post_by_id(post_id):
+    post = requests.get("https://jsonplaceholder.typicode.com/posts/" + post_id)
+    post_in_json = post.json()
+    response = app.response_class(response=json.dumps(post_in_json), status=200, mimetype='application/json')
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
