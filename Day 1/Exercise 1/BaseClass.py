6efd80f27e56f86473adc206d548483d8761e8ca
Alex Bloom <lehanbloom@gmail.com>
2020-05-17 14:53:18 +0300

    Day 1. Ex 2, #2. Add ExtendedClass, that inherits from BaseClass and adds a new var (date) in the constructor.

--- Day 1/Exercise 1/BaseClass.py
+++ Day 1/Exercise 1/BaseClass.py
@@ -1,8 +1,8 @@
 
 
 class BaseClass:
-    def __init__(self, userId, id, title, body):
-        self.userId = userId
+    def __init__(self, user_id, id, title, body):
+        self.user_id = user_id
         self.id = id
         self.title = title
         self.body = body
---
+++ Day 1/Exercise 1/ExtendedClass.py
@@ -0,0 +1,6 @@
+import BaseClass from BaseClass
+
+class ExtendedClass(BaseClass):
+    def __init__(self, created_at, user_id, id, title, body):
+        self.created_at = created_at
+        super().__init__(user_id, id, title, body)
