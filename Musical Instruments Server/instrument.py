d79320c2076e905d1cf2514129038d9c65d2ff63
Alex Bloom <lehanbloom@gmail.com>
2020-05-18 15:09:22 +0300

    Day 2. Created Flask app and class for deconstruction json we get from POST request.

---
+++ Musical Instruments Server/instrument.py
@@ -0,0 +1,11 @@
+import json
+
+class Instrument:
+    def __init__(self, dict):
+        self.instrument = dict.get("instrument")
+        self.user = dict.get("user")
+
+    def create_json(self):
+        return json.dumps(self.__dict__)
+
+
